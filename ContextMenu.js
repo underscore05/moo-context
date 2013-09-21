6fb7985a3dbeaaedc75657f8185a5ecc807ed621
Richard Roque <richardroque@com-mac.local>
2013-09-21 17:10:16 +0800

    Add another example usage of context menu

--- ContextMenu.js
+++ ContextMenu.js
@@ -1,65 +1,83 @@
 /**
-	script: ContextMenu.js
-	name: ContextMenu
-	description: Class that converts anchor tags inside the target element into a ContextMenu
-	license: MIT-style license
-	requires:
-		- Core/MooTools
-		- Core/More	
-		- Bootstrap.css
+script: ContextMenu.js
+name: ContextMenu
+description: Class that converts anchor tags inside the target element into a ContextMenu
+license: MIT-style license
+requires:
+- Core/MooTools
+- Core/More	
+- Bootstrap.css
 
-	Known limitations: 
-		- Doesn't work with nested element with nested Context menu
+Known limitations: 
+- Doesn't work with nested element with nested Context menu
 */
 var ContextMenu = new Class({
-	Implements: [Events, Options],
-	Binds: ["show", "hide"],
-	initialize: function(context, options) {
+    Implements: [Events, Options],
+    Binds: ["show", "hide"],
+	evt : null,
+    initialize: function(context, options) {
+        var self = this;
+        self.setOptions(options);
+        self.context = context;
+        
+		self.context.getElements('.actions').setStyle('display', 'none');
+        self.evt = 'contextmenu' + (self.options.relay ? ':relay(' + self.options.relay + ')': '');
+        self.context.addEvent(self.evt, self.show);
+        document.body.addEvent('click', self.hide);
+    },
+    show: function(evt, elem) {
+        document.body.fireEvent('click');
+        //Firing click will close other existing instances of ContextMenu
+        evt.stop();
+        var self = this;
+        if (!elem) {
+            elem = self.context;
+        }
+        var actions = elem.getElements('.actions a');
+        //Get previous sibling of .action element and attach Object to it.
+        elem.getElement('.actions !~').adopt(self.getContainer());
+        self.menuList.empty();
+        actions.each(function(a) {
+            var li = new Element('li');
+            li.adopt(a.clone());
+            self.menuList.adopt(li);
+        });
+        self.container.setStyle('opacity', 0);
+        self.container.setPosition(evt.page);
+        self.container.fade('in');
+        self.fireEvent('show', [evt, elem]);
+    },
+    hide: function(e) {
+        var self = this;
+        if (self.container) {
+            self.container.fade("out");
+        }
+    },
+    getContainer: function() {
+        var self = this;
+        if (!self.container) {
+            self.container = new Element('div.dropdown', {
+                'styles': {
+                    'position': 'absolute'
+                }
+            });
+            self.menuList = new Element('ul.dropdown-menu', {
+                'styles': {
+                    'display': 'block'
+                }
+            });
+            self.container.adopt(self.menuList);
+            self.container.get('tween').options.duration = 110;
+        }
+        return this.container;
+    },
+	dispose: function() {		
 		var self = this;
-		self.setOptions(options);               
-		self.context = context;
-		self.context.getElements('.actions').setStyle('display', 'none');               
-		var evt = 'contextmenu' + (self.options.relay?':relay('+self.options.relay+')':'');
-		self.context.addEvent(evt, self.show);
-		document.body.addEvent('click', self.hide);
-	},
-	show: function(evt, elem) {		
-		document.body.fireEvent('click'); //Firing click will close other existing instances of ContextMenu
-		evt.stop();
-		var self = this;
+		if(self.container) self.container.dispose();
+		self.context.removeEvent(self.evt, self.show);
+        document.body.removeEvent('click', self.hide);
 		
-		if(!elem) {
-			elem = self.context;
-		}
-		
-		var actions = elem.getElements('.actions a');
-		//Get previous sibling of .action element and attach Object to it.
-		elem.getElement('.actions !~').adopt(self); 
-		self.menuList.empty();
-		actions.each(function(a) {
-			var li = new Element('li');               
-			li.adopt(a.clone());
-			self.menuList.adopt(li);
-		});
-		self.container.setStyle('opacity', 0);
-		self.container.setPosition(evt.page);
-		self.container.fade('in');		
-		self.fireEvent('show', [evt, elem]);
-	},
-	hide: function(e) {
-		var self = this;		
-		if(self.container) {
-			self.container.fade("out");
-		}		
-	},
-	toElement: function() {
-		var self = this;
-		if(!self.container){
-			self.container = new Element('div.dropdown', {'styles':{'position':'absolute'}});
-			self.menuList = new Element('ul.dropdown-menu', {'styles':{'display':'block'}});
-			self.container.adopt(self.menuList);
-			self.container.get('tween').options.duration = 110;
-		}
-		return this.container;
+		self.context.getElements('.actions').setStyle('display', 'inherit');
+		delete this;	
 	}
 });
